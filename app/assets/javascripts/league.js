5febbc345728f51dbde47f620c33d193aa8cb0be
RJ Dabbar <rjdabbar@gmail.com>
2015-10-06 12:37:45 -0400

    backbone installed

---
+++ app/assets/javascripts/league.js
@@ -0,0 +1,13 @@
+window.League = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  League.initialize();
+});
