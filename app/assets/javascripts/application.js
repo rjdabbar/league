701963481d11df0339f062d093b3631eebd4c0d1
RJ Dabbar <rjdabbar@gmail.com>
2015-10-06 12:40:36 -0400

    added utility js files: composite view and serialize_json

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,13 +12,14 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require serialize_json
 //= require underscore
 //= require backbone
 //= require league
 //= require_tree ../templates
 //= require_tree ./models
 //= require_tree ./collections
+//= require_tree ./utils
 //= require_tree ./views
 //= require_tree ./routers
 //= require_tree .
